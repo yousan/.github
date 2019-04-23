08d614136972c850d879cf2944b70667ee1305a8
Yousan_O <yousan@l2tp.org>
2019-04-23 16:42:44 +0900

    update insstall.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # ISSUEテンプレートをダウンロードするスクリプトです。
 
-mkdir -p .github/ISSUE_TEMPLATE
+mkdir -fp .github/ISSUE_TEMPLATE
 
-curl -o .github/ISSUE_TEMPLATE/default.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md
-curl -o .github/ISSUE_TEMPLATE/detailed.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md
+curl -s -L -o .github/ISSUE_TEMPLATE/default.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/detailed.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/fix.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
+
+curl -s -L -o github/PULL_REQUEST_TEMPLATE.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md &
+wait
