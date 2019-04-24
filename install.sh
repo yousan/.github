be48d49bc9a325c627e5b9acd3e3ff0bcbb99cdf
Yousan_O <yousan@l2tp.org>
2019-04-24 11:41:06 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ curl -s -L -o .github/ISSUE_TEMPLATE/default.md https://raw.githubusercontent.co
 curl -s -L -o .github/ISSUE_TEMPLATE/detailed.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
 curl -s -L -o .github/ISSUE_TEMPLATE/fix.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
 
-curl -s -L -o github/PULL_REQUEST_TEMPLATE.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md &
+curl -s -L -o github/PULL_REQUEST_TEMPLATE.md https://raw.githubusercontent.com/yousan/github_template/master/.github/PULL_REQUEST_TEMPLATE.md &
 wait
