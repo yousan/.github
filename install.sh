1fd343c83ab32499fa50c3d95a8df3b264208c9a
Yousan_O <yousan@l2tp.org>
2019-04-25 12:51:01 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 
 mkdir -p .github/ISSUE_TEMPLATE
 
-curl -s -L -o .github/ISSUE_TEMPLATE/default.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md &
-curl -s -L -o .github/ISSUE_TEMPLATE/detailed.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
-curl -s -L -o .github/ISSUE_TEMPLATE/fix.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/default.md     https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/default_en.md  https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/default.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/detailed.md    https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/detailed_en.md https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
+curl -s -L -o .github/ISSUE_TEMPLATE/fix.md      https://raw.githubusercontent.com/yousan/github_template/master/.github/ISSUE_TEMPLATE/detailed.md &
 
 curl -s -L -o github/PULL_REQUEST_TEMPLATE.md https://raw.githubusercontent.com/yousan/github_template/master/.github/PULL_REQUEST_TEMPLATE.md &
 wait
